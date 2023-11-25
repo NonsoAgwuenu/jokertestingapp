03ae6c4ce6b87ca2c585b554852aa97550cd2e71
Nonso Agwuenu <aagwuenu@icloud.com>
2023-11-24 19:40:02 -0800

    Initial Commit

---
+++ JokerApptest/JokerApptestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokerApptestApp.swift
+//  JokerApptest
+//
+//  Created by Zo on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct JokerApptestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
